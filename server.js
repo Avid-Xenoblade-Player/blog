47ee808b830c3c922499c49d85ab3fb87d7b2259
juniper-ohyegods <142854385+Avid-Xenoblade-Player@users.noreply.github.com>
2024-08-08 10:21:42 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
 const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+const PORT = 3001;
+const BLOG_POSTS_DIR = path.join(__dirname, "blog-posts");
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Endpoint to get the list of blog posts
+app.get("/api/posts", (req, res) => {
+  fs.readdir(BLOG_POSTS_DIR, (err, files) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to read blog posts directory" });
+      return;
+    }
+    const txtFiles = files
+      .filter((file) => path.extname(file) === ".txt")
+      .sort();
+    res.json({ posts: txtFiles });
+  });
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// Serve the blog posts
+app.get("/blog-posts/:filename", (req, res) => {
+  const filePath = path.join(BLOG_POSTS_DIR, req.params.filename);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("File not found");
+    }
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
